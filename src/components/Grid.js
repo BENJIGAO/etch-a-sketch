c438ba3e62186df6d6c2ed94d1e9d6bdce172f3e
Brian Gao <bhgao20014@gmail.com>
2021-09-09 09:44:28 -0400

    Kind of figured out logic regarding the color and eraser changes

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,9 +5,29 @@ function Grid() {
   const arr = Array(99).fill(0)
   const sideLength = 10;
   const sideMeasure = '1fr '.repeat(sideLength);
+  const mode = "eraser"
+  function color(square) {
+    square.className = 'grid-square black';
+  }
+  function eraser(square) {
+    square.className = 'grid-square';
+  }
+  function currentMode(e) {
+    console.log(e.target)
+    if (mode === "color") {
+      color(e.target)
+    }
+    if (mode === "eraser") {
+      eraser(e.target)
+    }
+  }
+  
+  
   return (
     <div id="grid-container" style={{gridTemplateColumns: sideMeasure, gridTemplateRows: sideMeasure,}}>
-      {arr.map(friend => <div className="grid-square"></div>)}
+      {arr.map(tmp => (
+      <div className="grid-square" onMouseDown={currentMode}></div>
+      ))}
       
     </div>
   )
