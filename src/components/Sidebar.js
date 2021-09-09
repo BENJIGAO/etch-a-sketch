5df0fd08234345b6aeaa69fc1c83f4c880ca5ae4
Brian Gao <bhgao20014@gmail.com>
2021-09-09 14:19:36 -0400

    Added clear and rainbow function

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react'
 import "../component_styles/Sidebar.css"
 
 const Sidebar = () => {
-  const [mode, setMode] = useState("color black")
+  const [mode, setMode] = useState("")
   const gridSquares = document.querySelectorAll(".grid-square");
   gridSquares.forEach(gridSquare => {
     gridSquare.addEventListener("mouseover", initiateMode)
   })
 
   function initiateMode(e) {
+    console.log(e) 
     switch (mode.split(" ")[0]) {
       case "color":
         addColor(e, mode.split(" ")[1]);
@@ -25,18 +26,29 @@ const Sidebar = () => {
 
   function addColor(e, color) {
     e.target.className = `grid-square ${color}`;
+    e.target.style.backgroundColor = '';
+  }
+  
+  function addRainbow(e) {
+    e.target.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
   }
 
   function erase(e) {
     e.target.className = 'grid-square';
+    e.target.style.backgroundColor = '';
   }
 
-  function addRainbow(e) {
-    return
+  function clearGrid() {
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach((gridSquare) => {
+        gridSquare.className = 'grid-square';
+        gridSquare.style.backgroundColor = '';
+    })
   }
 
   return (
     <div>
+      <button onClick={clearGrid}>Clear</button>
       <button onClick={() => setMode("eraser")}>Eraser</button>
       <button onClick={() => setMode("color black")}>Black</button>
       <button onClick={() => setMode("color blue")}>Blue</button>
