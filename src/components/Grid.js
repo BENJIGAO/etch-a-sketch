cc14d6921b1f19f3fb29f06ac254417c642043db
Brian Gao <bhgao20014@gmail.com>
2021-09-09 08:29:55 -0400

    Figured out how to create grid squares; finally

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,10 +2,12 @@ import React from 'react'
 import "../component_styles/Grid.css"
 
 function Grid() {
+  const arr = Array(99).fill(0)
   const sideLength = 10;
   const sideMeasure = '1fr '.repeat(sideLength);
   return (
     <div id="grid-container" style={{gridTemplateColumns: sideMeasure, gridTemplateRows: sideMeasure,}}>
+      {arr.map(friend => <div className="grid-square"></div>)}
       
     </div>
   )
