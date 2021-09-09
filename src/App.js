8fbb6a94a5a61d960036291a6087c558208a8f01
Brian Gao <bhgao20014@gmail.com>
2021-09-09 10:11:03 -0400

    Figured out how to monitor mouse status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 import Grid from './components/Grid';
 function App() {
+  const [isMouseUp, setIsMouseUp] = useState(false)
+  function changeMouseStatus(e) {
+    switch (e.type) {
+      case "mouseup":
+        setIsMouseUp(true)
+        break;
+      case "mousedown":
+        setIsMouseUp(false);
+        break;
+    }
+  }
+  
   return (
-    <div className="App">
-      <Grid />
+    <div className="App" onMouseUp={changeMouseStatus} onMouseDown={changeMouseStatus}>
+      <Grid isMouseUp={isMouseUp}/>
     </div>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,32 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../component_styles/Grid.css"
 
-function Grid() {
+function Grid(props) {
+  console.log(props.isMouseUp)
+  const [isMouseDown, setIsMouseDown] = useState(false)
   const arr = Array(99).fill(0)
   const sideLength = 10;
   const sideMeasure = '1fr '.repeat(sideLength);
-  const mode = "eraser"
+  const mode = "color"
   function color(square) {
     square.className = 'grid-square black';
   }
-  function eraser(square) {
+  function erase(square) {
     square.className = 'grid-square';
   }
-  function currentMode(e) {
-    console.log(e.target)
+  function currentModeOnMouseDown(e) {
+    setIsMouseDown(true)
     if (mode === "color") {
       color(e.target)
     }
     if (mode === "eraser") {
-      eraser(e.target)
+      erase(e.target)
     }
+  }
+  function currentModeOnMouseOver(e) {
+    if (!isMouseDown) return
   }
   
   
   return (
     <div id="grid-container" style={{gridTemplateColumns: sideMeasure, gridTemplateRows: sideMeasure,}}>
       {arr.map(tmp => (
-      <div className="grid-square" onMouseDown={currentMode}></div>
+      <div className="grid-square" onMouseDown={currentModeOnMouseDown} onMouseEnter={currentModeOnMouseOver}></div>
       ))}
       
     </div>
